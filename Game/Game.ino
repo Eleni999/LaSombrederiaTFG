b831ad752be388806158dbe5360b9d35f5008717
Eleni999 <96084562+Eleni999@users.noreply.github.com>
2026-03-28 19:38:06 +0100

    ActualizacionGame
    Se ha integrado EscenaReloj como un .h en este  aun el reloj no sale en pantalla y prioriza el menu pero se han combinado los archivos con éxito.

--- Game/Game.ino
+++ Game/Game.ino
@@ -1,5 +1,3 @@
-
-
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <SPI.h>
@@ -9,21 +7,26 @@
 
 
 #include "BibliotecaImagenes.h"
+#include "EscenaReloj.h"
 
 U8G2_SSD1309_128X64_NONAME0_F_4W_SW_SPI u8g2(
   U8G2_R0, 10, 9, 12, 11, 13
 );
-
+EscenaReloj reloj;
 void setup() {
   u8g2.begin();
  u8g2.setBitmapMode(1);
+  reloj.begin(); 
 }
 
 void loop() {
+u8g2.clearBuffer();
 
- dibujarIconos(u8g2);
-u8g2.sendBuffer();
+reloj.update();
+dibujarIconos(u8g2); 
+reloj.draw(u8g2);
+
+u8g2.sendBuffer(); 
 delay(1000);
 
 }
-
