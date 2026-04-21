fe02e6e569dee2cb42ea21c647366240721ae925
Eleni999 <96084562+Eleni999@users.noreply.github.com>
2026-04-21 19:52:00 +0200

    Texto Té
    Incorporación del texto de las frases de Alicia con scroll lateral y tiempo para acabar la animación y primeros pasos ajustes.

--- Game/Game.ino
+++ Game/Game.ino
@@ -10,19 +10,15 @@
 #include "BibliotecaImagenes.h"
 #include "EscenaReloj.h"
 
-// Panralla
-U8G2_SSD1309_128X64_NONAME0_F_4W_SW_SPI u8g2(
-  U8G2_R0, 10, 9, 12, 11, 13
+// Pantalla
+U8G2_SSD1309_128X64_NONAME0_F_4W_SW_SPI u8g2(U8G2_R0, 10, 9, 12, 11, 13);
 
-);
-
-//Interruptor
-
+// Interruptor
 ezButton toggleSwitch(2); 
 bool sistemaEncendido = true;
 
-//Encoder 
-const int pinCLK =3;
+// Encoder 
+const int pinCLK = 3;
 const int pinDT = 4;
 const int pinSW = 5;
 int estadoAnteriorCLK;
@@ -34,27 +30,42 @@ int contadorClicks = 0;
 unsigned long ultimoClick = 0; 
 const unsigned long tiempoDobleClick = 400;
 
-//Reloj
-unsigned long tiempoPulsado =0;
+unsigned long ultimoCmabioEncoder = 0;
+const unsigned long debouceEncoder = 2;
+
+// Reloj
+unsigned long tiempoPulsado = 0;
 bool botonPresionado = false;
 bool estadoRReloj = true; 
 EscenaReloj reloj;
 
-//INICIO
+// INICIO
 unsigned long iniciarSistema = 0;
 bool arranqueCompleto = false;
 
-//Menu
+// Menu
 bool estadoMenu = false;
-//Led
+
+// Led
 #define PIN 6
 #define NUM_LEDS 7  
 Adafruit_NeoPixel strip(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
-int ledcentral =NUM_LEDS/6; 
+int ledcentral = NUM_LEDS/6; 
 uint32_t color;
 
-enum EstadoConejo {
+// Ajustes UI
+bool enAjustes = false;
+int velocidadAnimacion = 2; 
+int seleccionAjustes = 0; 
+int adjHora = 12, adjMin = 0, adjSeg = 0;
 
+// Té
+unsigned long duracionTe = 9000;
+unsigned long inicioTe = 0;
+int indiceFraseAleatoria= 0;
+int scrollX = 128;
+
+enum EstadoConejo {
     IDE,
     BANAR,
     COMER,
@@ -63,281 +74,312 @@ enum EstadoConejo {
     MUERTE,
     AJUSTES,
     FELIZ
- };
- enum EstadoBase{
-  BASE_IDE,
-  BASE_HAMBRE
- };
- struct Necesidadades {
-   int hambre = 30;
-   int sueno = 30;
-   int sucio = 30; 
- };
+};
 
-//EstadosConejo
+enum EstadoBase {
+    BASE_IDE,
+    BASE_HAMBRE
+};
+
+struct Necesidadades {
+    int hambre = 30;
+    int sueno = 30;
+    int sucio = 30; 
+};
+
 EstadoConejo estadoActual = IDE;
-EstadoBase baseActual =  BASE_IDE;
+EstadoBase baseActual = BASE_IDE;
 unsigned long ultimoTick = 0;
 Necesidadades necesidades; 
 bool necesidadHambre = false;
 bool necesidadSueno = false; 
 bool necesidadSucio = false; 
 
-unsigned long  tiempoEstado = 0;
-unsigned long  duracionEstado = 3000;
+unsigned long tiempoEstado = 0;
+unsigned long duracionEstado = 3000;
 bool estadoCurso = false; 
 
-void sistemaInterruptor(){
-  toggleSwitch.loop();
-  sistemaEncendido = toggleSwitch.getState();
+void manejarScrollTe(U8G2 &u8g2, int fraseIdx) {
+    const char* frase = obtenerFrase(fraseIdx); 
+    u8g2.setFont(u8g2_font_5x7_tr);
+    int anchoTexto = u8g2.getStrWidth(frase);
+    
+    unsigned long transcurrido = millis() - tiempoEstado;
+
+    float progreso = (float)transcurrido / (float)duracionEstado;
+    scrollX = 128 - (progreso * (128 + anchoTexto));
+    
+    u8g2.drawStr(scrollX, 63, frase);
 }
+// Prototipos de funciones nuevas para UI
+void dibujoAjustesUI(U8G2 &u8g2);
+void manejarEntradaAjustes();
+
+void sistemaInterruptor() {
+    toggleSwitch.loop();
+    sistemaEncendido = toggleSwitch.getState();
+}
+
 void setup() {
-  u8g2.begin();
- u8g2.setBitmapMode(1);
- reloj.begin(); 
+    u8g2.begin();
+    u8g2.setBitmapMode(1);
+    reloj.begin(); 
 
-toggleSwitch.setDebounceTime(50);
+    toggleSwitch.setDebounceTime(50);
 
- // Pines
- pinMode(pinCLK, INPUT_PULLUP);
- pinMode(pinDT, INPUT_PULLUP);
- pinMode(pinSW, INPUT_PULLUP);
- estadoAnteriorCLK = digitalRead(pinCLK);
- 
-  strip.begin();
-  strip.show();
+    pinMode(pinCLK, INPUT_PULLUP);
+    pinMode(pinDT, INPUT_PULLUP);
+    pinMode(pinSW, INPUT_PULLUP);
+    estadoAnteriorCLK = digitalRead(pinCLK);
+    
+    strip.begin();
+    strip.show();
 
-  iniciarSistema = millis();
-  estadoRReloj = true;
-
-};
+    iniciarSistema = millis();
+    estadoRReloj = true;
+}
 
 void loop() {
+    sistemaInterruptor();
 
- sistemaInterruptor();
-
-  sistemaInterruptor();
-
-  if(!sistemaEncendido){
-    u8g2.clearBuffer();
-    u8g2.sendBuffer();
-
-    strip.clear();
-    strip.show();
-    return; 
-  }
-  u8g2.clearBuffer();
-
-Menu();
-Reloj();
-if ( !arranqueCompleto &&  millis()-iniciarSistema >=4000 ){
-  arranqueCompleto = true;
-  estadoRReloj = false; 
-  estadoActual = IDE;
-}
-if ( !estadoRReloj && !estadoMenu ){
- acturlizarNecesidades();
- //interccionEstados();
- actualizarEstadoIA();
-}
-
-if (estadoCurso){
-  if( millis()- tiempoEstado >=duracionEstado){
-    estadoCurso = false;
-    estadoActual = IDE;
-  }
-}
-
-if (estadoRReloj ){
-  reloj.update();
-  reloj.draw(u8g2);
-
-}else if(estadoMenu){
-dibujarIconos(u8g2, seleccion);
-}
-else{
-  
- switch (estadoActual){
-  case IDE:
-  if(baseActual == BASE_HAMBRE){
-     dibujarHambre(u8g2);
-  } else{
-   dibujoIDE(u8g2);
-   color = strip.Color(242, 226, 167);
-  }
-   if (necesidadSucio){ 
-  dibujoSucio(u8g2);
-   color = strip.Color(91, 146, 235);} 
-   if(necesidadSueno){
-    dibujarSueno(u8g2);
-   }
-  break;
-  case FELIZ:
-  dibujarFeliz(u8g2);
-  color = strip.Color(242, 226, 167);
-  break;
-
-  case BANAR:
-  dibujarBano(u8g2);
-  break;
-  case COMER:
-  dibujarComer(u8g2);
-  break;
-  case DORMIR:
-  dibujarDormir(u8g2);
-  break;
-  case MUERTE:
-  dibujarMuerte(u8g2);
-  break;
-  case TE:
-  dibujarTe(u8g2);
-  break;
-
- }
-}
-
-u8g2.sendBuffer(); 
-delay(50);
-
-}
-void Menu(){
-
-  if (!sistemaEncendido) return; 
-
-  estadoActualCLK = digitalRead(pinCLK);
-  
-
-  if (estadoAnteriorCLK ==HIGH && estadoActualCLK== LOW ){
-    if(digitalRead(pinDT) == HIGH){
-      seleccion ++;
-      
-    }else {
-      seleccion --;
-    }
-    if ( seleccion > 4) seleccion  =0;
-    if (seleccion < 0) seleccion = 4; 
-  }
-  estadoAnteriorCLK  = estadoActualCLK;
- 
-
-}
-void Reloj(){
-
-  if (!sistemaEncendido) return; 
- estadoActualSW = digitalRead(pinSW);
-
-  if (estadoActualSW == LOW) {
-    if (!botonPresionado) {
-      tiempoPulsado = millis();
-      botonPresionado = true;
-
-      if (millis() - ultimoClick < tiempoDobleClick) { 
-        contadorClicks ++; 
-      } else {
-        contadorClicks = 1; 
-      }
-      ultimoClick = millis(); 
-
-      strip.setPixelColor(ledcentral, strip.Color(4, 23, 48));
-    }
-    else {
-      if (!arranqueCompleto && millis() - tiempoPulsado >= 2000) {
-        estadoRReloj = false; 
-        arranqueCompleto = true;
-        estadoActual = IDE;
-        botonPresionado = false;
+    if (!sistemaEncendido) {
+        u8g2.clearBuffer();
+        u8g2.sendBuffer();
         strip.clear();
-      }
+        strip.show();
+        return; 
     }
-  }
-  else {
-    botonPresionado = false;
-  }
 
-  if (!estadoRReloj && contadorClicks == 2 && millis() - ultimoClick < tiempoDobleClick) { 
-    estadoMenu = true; 
-    estadoCurso = false; 
-    contadorClicks = 0; 
-  }
-  if(!estadoRReloj && estadoMenu && contadorClicks == 1 && millis() - ultimoClick > tiempoDobleClick){
-  interccionEstados(); 
-  contadorClicks = 0;
- }
-}
- void interccionEstados(){
-  
-  if (!sistemaEncendido) return; 
-  estadoMenu = false;
-
-  tiempoEstado = millis();
-  estadoCurso = true;
-    switch (seleccion){
-
-    case 0:
-      estadoActual = TE;
-    break;
-
-    case 1:
-      estadoActual = COMER;
-      necesidades.hambre +=12;
-      if(necesidades.hambre > 30) necesidades.hambre = 30;
-    break;
-
-    case 2:
-      estadoActual = DORMIR;
-      necesidades.sueno +=15;
-      if(necesidades.sueno > 30) necesidades.sueno = 30;
-    break;
-
-    case 3:
-      estadoActual = BANAR;
-      necesidades.sucio =30;;
-      if(necesidades.sucio > 30) necesidades.sucio = 30;
-    break;
-
-    case 4:
-      estadoActual = AJUSTES;
-    break;
-  }
-  necesidades.hambre = constrain(necesidades.hambre, 0, 32);
-  necesidades.sueno  = constrain(necesidades.sueno, 0, 32);
-  necesidades.sucio  = constrain(necesidades.sucio, 0, 30);
- }
- void acturlizarNecesidades(){
-  if (estadoCurso) return; 
-  
-  if ((millis()-ultimoTick) >=50000){
-    ultimoTick = millis();
-    necesidades.hambre--;
-    necesidades.sueno --;
-    necesidades.sucio --;
-  }
- }
- void actualizarEstadoIA(){
+    u8g2.clearBuffer();
    
-   if(estadoCurso) return; 
-   //muerte
-   if( necesidades.hambre <= 0 || necesidades.sueno <=0 || necesidades.sucio <=0){
+    
+    Menu();
+    Reloj();
 
-    estadoActual = MUERTE;
-    return;
-   }
+    if (!arranqueCompleto && millis() - iniciarSistema >= 4000) {
+        arranqueCompleto = true;
+        estadoRReloj = false; 
+        estadoActual = IDE;
+    }
+
+    if (!estadoRReloj && !estadoMenu && !enAjustes) {
+        acturlizarNecesidades();
+        actualizarEstadoIA();
+    }
+
+    if (estadoCurso) {
+        if (millis() - tiempoEstado >= duracionEstado) {
+            estadoCurso = false;
+            estadoActual = IDE;
+        }
+    }
+
+    if (estadoRReloj) {
+        reloj.update();
+        reloj.draw(u8g2);
+    } else if (estadoMenu) {
+        dibujarIconos(u8g2, seleccion);
+    } else {
+        switch (estadoActual) {
+            case IDE:
+                if (baseActual == BASE_HAMBRE) {
+                    dibujarHambre(u8g2);
+                } else {
+                    dibujoIDE(u8g2);
+                    color = strip.Color(242, 226, 167);
+                }
+                if (necesidadSucio) { 
+                    dibujoSucio(u8g2);
+                    color = strip.Color(91, 146, 235);
+                } 
+                if (necesidadSueno) {
+                    dibujarSueno(u8g2);
+                }
+                break;
+            case FELIZ:
+                dibujarFeliz(u8g2);
+                color = strip.Color(242, 226, 167);
+                break;
+            case BANAR:
+                dibujarBano(u8g2);
+                break;
+            case COMER:
+                dibujarComer(u8g2);
+                break;
+            case DORMIR:
+                dibujarDormir(u8g2);
+                break;
+            case MUERTE:
+                dibujarMuerte(u8g2);
+                break;
+            case TE:
+                manejarScrollTe(u8g2, indiceFraseAleatoria);
+                dibujarTe(u8g2);
+                break;
+            case AJUSTES:
+               
+                 dibujoAjustesUI(u8g2, seleccionAjustes, adjHora, adjMin, adjSeg);
+               
+                break;
+        }
+    }
+
+    u8g2.sendBuffer();
+  delay(10);
+}
+
+void manejarEntradaAjustes() {
+    switch (seleccionAjustes) {
+        case 0: velocidadAnimacion = 1; break; // Lento
+        case 1: velocidadAnimacion = 5; break; // Medio
+        case 2: velocidadAnimacion = 10; break; // Rapido
+        case 3: adjHora = (adjHora + 1) % 24; break;
+        case 4: adjMin = (adjMin + 1) % 60; break;
+        case 5: adjSeg = (adjSeg + 1) % 60; break;
+        case 6: // Aceptar
+     
+            reloj.setTime(adjHora, adjMin, adjSeg);
+            enAjustes = false;
+            estadoActual = IDE;
+            break;
+    }
+}
+
+void Menu() {
+    if (!sistemaEncendido) return; 
+    estadoActualCLK = digitalRead(pinCLK);
+    if (estadoActualCLK != estadoAnteriorCLK) {
+        if (millis() - ultimoCmabioEncoder >= debouceEncoder) {
+            ultimoCmabioEncoder = millis();
+            if (estadoActualCLK == HIGH) {
+                if (enAjustes) {
+                    if (digitalRead(pinDT) != estadoActualCLK) seleccionAjustes++;
+                    else seleccionAjustes--;
+                    seleccionAjustes = constrain(seleccionAjustes, 0, 6);
+                }
+                else if (estadoMenu) {
+                    if (digitalRead(pinDT) != estadoActualCLK) seleccion++;
+                    else seleccion--;
+                    if (seleccion > 4) seleccion = 0;
+                    if (seleccion < 0) seleccion = 4;
+                }
+            }
+        }
+    }
+    estadoAnteriorCLK = estadoActualCLK;
+}
+
+void Reloj() {
+if (!sistemaEncendido) return; 
+    estadoActualSW = digitalRead(pinSW);
+
+    if (estadoActualSW == LOW) {
+        if (!botonPresionado) {
+            botonPresionado = true;
+            tiempoPulsado = millis();
+            if (millis() - ultimoClick < tiempoDobleClick) contadorClicks++;
+            else contadorClicks = 1;
+            ultimoClick = millis(); 
+        } else {
+            if (!arranqueCompleto && (millis() - tiempoPulsado >= 2000)) {
+                estadoRReloj = false; 
+                arranqueCompleto = true;
+                botonPresionado = false;
+            }
+        }
+    } 
+    else { 
+        if (botonPresionado) {
+            if (enAjustes) manejarEntradaAjustes();
+            botonPresionado = false;
+        }
+    }
+
+    unsigned long tiempoTranscurrido = millis() - ultimoClick;
+    if (contadorClicks == 1 && tiempoTranscurrido > tiempoDobleClick) {
+        if (estadoMenu && !enAjustes) interccionEstados(); 
+        else if (estadoRReloj) { /* Salir de protector de pantalla con un click */
+             estadoRReloj = false;
+        }
+        contadorClicks = 0; 
+    }
+    if (contadorClicks == 2) {
+        if (!estadoRReloj) {
+            estadoMenu = !estadoMenu; 
+            enAjustes = false;
+        }
+        contadorClicks = 0;
+    }
+}
+
+
+
+void interccionEstados() {
+    estadoMenu = false;
+    estadoCurso = true;
+    tiempoEstado = millis();
+   // if (!sistemaEncendido) return; 
+
+    switch (seleccion) {
+        case 0: estadoActual = TE; 
+            duracionEstado = 6000; 
+            indiceFraseAleatoria = random(0, 11);; break;
+        case 1:
+            estadoActual = COMER;
+            necesidades.hambre += 12;
+            if (necesidades.hambre > 30) necesidades.hambre = 30;
+            break;
+        case 2:
+            estadoActual = DORMIR;
+            necesidades.sueno += 15;
+            if (necesidades.sueno > 30) necesidades.sueno = 30;
+            break;
+        case 3:
+            estadoActual = BANAR;
+            necesidades.sucio = 30;
+            break;
+        case 4:
+            estadoActual = AJUSTES;
+            enAjustes = true;
+            estadoCurso = false;
+            seleccionAjustes = 0;
+            break;
+    }
+    necesidades.hambre = constrain(necesidades.hambre, 0, 32);
+    necesidades.sueno = constrain(necesidades.sueno, 0, 32);
+    necesidades.sucio = constrain(necesidades.sucio, 0, 30);
+}
+
+void acturlizarNecesidades() {
+  if (estadoCurso) return; 
+    if ((millis() - ultimoTick) >= 50000) {
+        ultimoTick = millis();
+        necesidades.hambre--;
+        necesidades.sueno--;
+        necesidades.sucio--;
+    }
+}
+
+void actualizarEstadoIA() {
+    if (estadoCurso || enAjustes || estadoActual == AJUSTES) return; 
+    if (necesidades.hambre <= 0 || necesidades.sueno <= 0 || necesidades.sucio <= 0) {
+        estadoActual = MUERTE;
+        return;
+    }
     necesidadHambre = necesidades.hambre < 15;
     necesidadSueno = necesidades.sueno < 15;
-    necesidadSucio = necesidades.sucio< 15;
+    necesidadSucio = necesidades.sucio < 15;
     
-   if(necesidadHambre ){
-     baseActual =  BASE_HAMBRE;
-   }else{
-     baseActual =  BASE_IDE;
-   }
-   if(necesidades.hambre >= 30 && necesidades.sueno >= 30 && necesidades.sucio >= 30){
-    estadoActual = FELIZ;
-    return;
-   }
-    if (estadoActual != COMER && estadoActual != DORMIR && estadoActual != BANAR && estadoActual != TE && estadoActual != FELIZ && estadoActual != MUERTE){
-       estadoActual = IDE;
+    if (necesidadHambre) {
+        baseActual = BASE_HAMBRE;
+    } else {
+        baseActual = BASE_IDE;
     }
-    
-  }
-
+    if (necesidades.hambre >= 30 && necesidades.sueno >= 30 && necesidades.sucio >= 30) {
+        estadoActual = FELIZ;
+        return;
+    }
+    if (estadoActual != COMER && estadoActual != DORMIR && estadoActual != BANAR && estadoActual != TE && estadoActual != FELIZ && estadoActual != MUERTE) {
+        estadoActual = IDE;
+    }
+}
